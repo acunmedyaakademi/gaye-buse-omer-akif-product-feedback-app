2d0f1e5234379aa7f49323d8e569fc4bed0f716a
[Gaye Dinç] <gayedinc190@gmail.com>
2025-02-13 14:42:27 +0300

    hamburger menü oluşturuldu

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -4,6 +4,7 @@ import { FeedbackContext } from "./FeedbackContext";
 export default function Suggestions() {
   const { feedbacks } = useContext(FeedbackContext);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Hamburger menü için state
 
   useEffect(() => {
     const handleResize = () => {
@@ -13,6 +14,11 @@ export default function Suggestions() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  // Hamburger menüyü aç/kapat
+  function hamburgerMenu() {
+    setIsMenuOpen((prevState) => !prevState);
+  }
+
   return (
     <div className="suggestionsPage">
       {isMobile ? (
@@ -21,7 +27,34 @@ export default function Suggestions() {
             <h5>Frontend Mentor</h5>
             <p>Feedback Board</p>
           </div>
-          <img src="svg/hamburgerIcon.svg" alt="hamburger icon" />
+          <div className="hamburger-menu">
+            <img
+              src={isMenuOpen ? "svg/hamburgerCloseIcon.svg" : "svg/hamburgerIcon.svg"}
+              alt="Hamburger Menu"
+              onClick={hamburgerMenu}
+              className={isMenuOpen ? "hamburger-icon-none" : ""}
+            />
+          </div>
+          <div className={`hamburger-menu-overlay ${isMenuOpen ? "block" : "none"}`}>
+            <div className={`hamburger-menu-content ${isMenuOpen ? "block" : "none"}`}>
+              <div className="categories">
+                <button>All</button>
+                <button>UI</button>
+                <button>UX</button>
+              </div>
+              <div className="roadmap">
+                <div className="roadmap-head">
+                  <h3>Roadmap</h3>
+                  <button>View</button>
+                </div>
+                <div className="roadmap-status">
+                  <span>Planned</span>
+                  <span>In-Progress</span>
+                  <span>Live</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="suggestionsPageHeader">
